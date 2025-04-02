5f7fcf14c524ab9322938103c86090933b2f35d3
gpPacheco <gabrielfppacheco@gmail.com>
2025-04-01 21:59:50 -0300

    Adicionar mensagem de boas-vindas e link para a página Sobre na Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 
 function Home() {
     return (
       <div >
-        <h1>Bem-vindo a página Home</h1>
+        <h1>Bem-vindo a página Home</h1> <br />
+        <span> Nossa primeira aplicação em React</span> <br /> <br />
+
+        <Link to="/sobre">Sobre  </Link>
       </div>
     );
   }
