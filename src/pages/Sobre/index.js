7df81180a36afd2cbe4b7849751a261a5b8042e9
gpPacheco <gabrielfppacheco@gmail.com>
2025-04-08 21:53:18 -0300

    Adicionar link de navegação para a Home na página Sobre

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 
 function Sobre() {
     return (
       <div >
-        <h1>Pagina sobre a empresa</h1>
+        <h1>Pagina sobre a empresa</h1> <br />
+        <Link to='/'>Voltar para a Home</Link>
       </div>
     );
   }
