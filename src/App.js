844e592f53a10fe6fff457d44552fbbb6a2e4f40
gpPacheco <gabrielfppacheco@gmail.com>
2025-04-09 21:49:58 -0300

    Adicionar componente Header e ajustar estrutura do App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import RoutesApp from './routes';
-
+import Header from './components/Header';
 
 function App() {
   return (
-    <div >
+    < >
+      <Header /> <br />
       <RoutesApp />
-    </div>
+    </>
   );
 }
 
