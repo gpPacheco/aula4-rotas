d3d76cfc522d364c5f7fe2def50f9ccf588f0316
gpPacheco <gabrielfppacheco@gmail.com>
2025-04-10 21:52:29 -0300

    Criação do componente header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+import './style.css';
+
 function Header(){
     return(
         <header>
             <h1>My Header</h1>
+
+            <div className='menu'>
+                <Link to="/">Home</Link>
+                <Link to="/sobre">About</Link>
+            </div>
         </header>
     )
 }
